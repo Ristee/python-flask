d130263f5c7ca8359b06fb555cacecd44b8744db
ristee <rristee@gmail.com>
2024-08-27 15:36:10 +0500

    add templates

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flask Web Application
+
+This is a web application created by following a video tutorial
+
+## Links
+- Source https://youtu.be/dam0GPOAvVI
+- Github https://github.com/techwithtim/Flask-Web-App-Tutorial
+
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
+    data = request.form
+    return render_template('login.html')
 
 
 @auth.route('/logout')
 def logout():
     return 'logout'
 
-@auth.route('/signup')
+
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return 'signup'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
+
+    return render_template('signup.html')
